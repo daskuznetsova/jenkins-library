0a6c5350094da181c14d7a549b0b5f683d9a7952
Daria Kuznetsova <d.kuznetsova@sap.com>
2024-03-15 17:14:40 +0100

    refactored tests

--- pkg/codeql/flags_test.go
+++ pkg/codeql/flags_test.go
@@ -1,1 +1,68 @@
 package codeql
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateFlags(t *testing.T) {
+	t.Parallel()
+
+	t.Run("All flags are valid", func(t *testing.T) {
+		input := map[string]string{
+			"--flag1": "1",
+			"--flag2": "2",
+			"--flag3": "3",
+		}
+		flags := map[string]bool{"--flag1": true, "--flag2": true, "--flag3": true}
+		expected := []string{"--flag1=1", "--flag2=2", "--flag3=3"}
+		result, err := ValidateFlags(input, flags)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, result)
+	})
+	t.Run("No valid flags", func(t *testing.T) {
+		input := map[string]string{
+			"--flag1": "1",
+			"--flag2": "2",
+			"--flag3": "3",
+		}
+		flags := map[string]bool{}
+		expected := []string{}
+		result, err := ValidateFlags(input, flags)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, result)
+	})
+	t.Run("Some flags are valid", func(t *testing.T) {
+		input := map[string]string{
+			"--flag1": "1",
+			"--flag2": "2",
+			"--flag3": "3",
+		}
+		flags := map[string]bool{"--flag1": true, "--flag3": true}
+		expected := []string{"--flag1=1", "--flag3=3"}
+		result, err := ValidateFlags(input, flags)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, result)
+	})
+	t.Run("Flags without values", func(t *testing.T) {
+		input := map[string]string{
+			"--flag1": "",
+			"--flag2": "",
+			"--flag3": "",
+		}
+		flags := map[string]bool{"--flag1": true, "--flag2": true, "--flag3": true}
+		expected := []string{"--flag1", "--flag2", "--flag3"}
+		result, err := ValidateFlags(input, flags)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, result)
+	})
+	t.Run("Empty input", func(t *testing.T) {
+		input := map[string]string{}
+		flags := map[string]bool{"--flag1": true, "--flag2": true, "--flag3": true}
+		expected := []string{}
+		result, err := ValidateFlags(input, flags)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, result)
+	})
+}
